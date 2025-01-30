191151b4a4e8b77f15f541627e221d6c9c9e43d8
Shivaniramesh1304 <shivaniramesh1304@gmail.com>
2025-01-30 21:35:25 +0530

    Add files via upload

---
+++ Evenoddcheck.java
@@ -0,0 +1,8 @@
+public class Evenoddcheck {
+    public static void main(String[]args){
+        int number=7;
+        String result=(number%2==0)?"even":"odd";
+        System.out.println("the number"+number+"is"+result+".");
+    }
+    
+}
